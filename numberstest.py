87b4fb9dc55d92ad618c15a55e4bd20bb86cb7e2
Traccykay <traccykay@gmail.com>
2017-03-07 12:46:52 +0300

    Update numberstest.py

--- numberstest.py
+++ numberstest.py
@@ -5,15 +5,15 @@ class prime(unittest.TestCase):
 	"""docstring for TestCase"""
 	
 	def test_is_one_prime(self):
-		self.assertTrue(prime_no(1))
+		self.assertFalse(prime_no(1))
 	def test_is_two_prime(self):
 		self.assertTrue(prime_no(2))
 	def test_is_a_prime(self):
-		self.assertTrue(prime_no("a"))
+		self.assertFalse(prime_no('v'))
 	def test_is_a_negative_prime(self):
-		self.assertTrue(prime_no(-5))
+		self.assertFalse(prime_no(-5))
 	def test_is_a_float_prime(self):
-		self.assertTrue(prime_no(8.5))
+		self.assertFalse(prime_no(8.5))
 
 if __name__=="__main__":
 	unittest.main()
