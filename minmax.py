46250297cd3cb1d1ae796f10a0636a60bb4897bf
Traccykay <traccykay@gmail.com>
2017-03-09 16:04:07 +0300

    Final Commit

--- minmax.py
+++ minmax.py
@@ -1,13 +1,19 @@
-def find_max_min(a):
-	if type (a) == list:
-		minimum = min(a)
-		maximum = max(a)
-
-	if min==max:
-		b = len(a)
-		return[b]
-	else:
-		return (maximum, minimum)
-
-
-	
+def find_max_min(number):
+  minimum = number[0]
+  maximum = number[1]
+  if type(number) == list:
+    for numbers in number:
+      if numbers < minimum:
+        minimum = numbers
+      elif numbers > maximum:
+        maximum = numbers
+    b = []
+    if minimum == maximum:
+      b.append(len(number))
+      return b
+    else:
+      b.append(minimum)
+      b.append(maximum)
+      return b
+  else:
+    return "The input is not a list"
